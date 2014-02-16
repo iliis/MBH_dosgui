9347d9a964ff16933794f6a1eaa3aa350428092a
iliis <samuelbryner@gmx.ch>
2014-02-16 18:09:27 +0100

    PDCurses is linking and working!
    What a PITA. OpenWatcom isn't as polished as GCC...

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 CC=wcl
-CFLAGS=-l=dos -cc++
+CFLAGS=-l=dos -cc++ -i=pdcurses -wx -ml
 
 .PHONY: clean all cleantest
 
 all:	main.c
-	$(CC) $(CFLAGS) main.c
+	$(CC) $(CFLAGS) pdcurses/pdcurses.lib main.c
 
 test:	test.c
 	$(CC) $(CFLAGS) test.c
 
 clean:	cleantest
+	-@rm -f main.o main.exe
 
 cleantest:
 	-@rm -f test.o test.exe
--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdlib.h>
 #include <iostream.h>
 
+// notes on compiling pcurses with watcom on linux
+// owsetenv.sh:
+/*
+ 
+#!/bin/sh
+echo Open Watcom Build Environment
+export WATCOM=/home/samuel/programme/watcom
+export PATH=/home/samuel/programme/watcom/binl:$PATH
+# h and NOT lh !
+export INCLUDE=/home/samuel/programme/watcom/h:$INCLUDE
+export EDPATH=/home/samuel/programme/watcom/eddat
+export WIPFC=/home/samuel/programme/watcom/wipfc
+export LIB=/home/samuel/programme/watcom/lib286/dos:$LIB
+
+*/
+// makefile: replace / with - in parameters, replace copy/del with cp/rm
+
+
+
+
+#include <curses.h>
+
 int main() {
 	cout << "foobar" << endl;
 	cin.ignore();
+
+	initscr();
+	printw("Hallo Welt2");
+	refresh();
+
+	cin.ignore();
+	endwin();
 
 	return EXIT_SUCCESS;
 }
